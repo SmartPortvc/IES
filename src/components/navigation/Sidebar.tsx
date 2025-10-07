b39feb501e3112ff5c9ea6b9ca6f0d966a5bdbe5
SmartPortvc <smartportvc@gmail.com>
2025-10-07 20:47:02 +0530

    Updated Sidebar.tsx

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -78,7 +78,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeItem, setActiveItem }) => {
         }`}
       >
         <img
-          src="https://apmaritime.in/images/13_apmb_logo.png"
+          src="https://apmaritime.in/assets/apmb-B0Lm-J0A.png"
           alt="APMB Logo"
           className={`${
             collapsed ? "h-10 w-10" : "h-12 w-12"
